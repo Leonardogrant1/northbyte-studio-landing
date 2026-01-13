1650cdfed1646d32968c40a869d0d9f40f98cadd
Leonardogrant1 <LeonardoGranetto@gmail.com>
2026-01-13 19:17:01 +0100

    remove sensitive and generated files

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"4YMcI4o59MMzP0B8gr8hPbSAz5QwwhlsB0QGDfCEOVY=\"\n}"
--- .next/static/8eoiuhQSCk3bd_bJrvc37/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fprivacy-policy\u002F[app]","\u002Fterms-of-use\u002F[app]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
